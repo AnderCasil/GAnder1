a75446a9cb764d6164a6fb888db8547e32a514c6
Ander <Ander@DESKTOP-ROV3615.lan>
2024-04-12 18:30:00 +0200

    Tercer Commit

--- proAnder/src/prueba/Prueba1.java
+++ proAnder/src/prueba/Prueba1.java
@@ -11,6 +11,6 @@ package prueba;
 public class Prueba1 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Adios Mundo");
+        System.out.println("No mas mundo");
     }
 }
